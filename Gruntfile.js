ad7abffdfceb0d20835e37c468b27463fde9af30
Christopher Hiller <boneskull@boneskull.com>
2015-07-02 23:07:42 -0700

    add mocha_istanbul task; add fixture; fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,40 @@ module.exports = function (grunt) {
   var loadGruntConfig = require('load-grunt-config');
   var pkg = grunt.file.readJSON('package.json');
 
+  /**
+   * Random bits of crap to send to the Grunt templates
+   * @type {{pkg: Object, bower: ?Object, min: Function, author: *}}
+   */
+  var data = {
+    pkg: pkg,
+    author: typeof pkg.author === 'string' ? pkg.author :
+      [pkg.author.name, pkg.author.email].join(' ')
+  };
+
+  Object.defineProperty(data, 'author', {
+    /**
+     * Normalizes `author` field of `package.json`.
+     * @returns {string} Author name(s) and email(s)
+     */
+    get: function author() {
+      function _author(author) {
+        var format;
+        if (typeof author === 'string') {
+          return author;
+        }
+        format = require('util').format;
+        return format('%s <%s>', author.name, author.email);
+      }
+
+      if (Array.isArray(pkg.author)) {
+        return pkg.author.map(function (author) {
+          return _author(author);
+        }).join(', ');
+      }
+      return _author(pkg.author);
+    }
+  });
+
   if (grunt.option('time')) {
     require('time-grunt')(grunt);
   }
@@ -14,25 +48,10 @@ module.exports = function (grunt) {
         devUpdate: 'grunt-dev-update',
         'bump-only': 'grunt-bump',
         'bump-commit': 'grunt-bump',
-        mochacov: 'grunt-mocha-cov'
+        'mocha_istanbul': 'grunt-mocha-istanbul'
       }
     },
-    data: {
-      pkg: pkg,
-      bower: grunt.file.readJSON('bower.json'),
-
-      /**
-       * Dumb little function to generate a foo.min.xxx filename from foo.xxx
-       * @param {string} filepath Filepath
-       * @returns {string} Minified filepath
-       */
-      min: function min(filepath) {
-        var path = require('path');
-        var ext = path.extname(filepath);
-        return path.basename(filepath, ext) + '.min' + ext;
-      },
-      author: typeof pkg.author === 'string' ? pkg.author :
-        [pkg.author.name, pkg.author.email].join(' ')
-    }
+    data: data
   });
+
 };
---
+++ test/fixture.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var chai = require('chai');
+
+global.expect = chai.expect;
+global.sinon = require('sinon');
+
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
+
+require('bluebird').longStackTraces();
